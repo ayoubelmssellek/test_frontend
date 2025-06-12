9f0c79873dc8d5f8b3d02e83b4f53806b980ca99
Ayoub El mssellek <ayoubmssallak7@gmail.com>
2025-06-12 10:09:32 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'Gusto',
+        name: 'Gusto Dakhla',
+        icons: [
+          {
+            src: '/gustologo.jpg',
+            sizes: '512x512',
+            type: 'image/jpeg',
+          }
+        ],
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ff5722',
+        background_color: '#ffffff',
+      }
+    })
+  ],
   server: {
     proxy: {
       '/api': {
-        target: 'https://laravelapi-production-64b8.up.railway.app', // العنوان ديال Laravel
+        target: 'https://laravelapi-production-64b8.up.railway.app',
         changeOrigin: true,
         secure: false,
       },
